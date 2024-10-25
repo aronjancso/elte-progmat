6b8521b1df5aad4e76cc86e5bb0e6a27913760fa
Aron Jancso <aron.jancso@flowject.hu>
2024-10-25 15:19:25 +0200

    refactor: separate input read and logic

--- 01-programozas-ip-18eprogeg/bedando/c1/c1-megoldas/Program.cs
+++ 01-programozas-ip-18eprogeg/bedando/c1/c1-megoldas/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace C1
+﻿/*
+ * Hallgató: Jancsó Áron
+ * Neptun:   I8MV9H
+ */
+
+using System;
+
+namespace C1
 {
     internal class Program
     {
@@ -11,39 +18,41 @@
 
         static void Main(string[] args)
         {
-            int n;
+            // Deklaracio
+            int     n;
             Ember[] emberek;
-            Ember keresett;
-            int index;
+            Ember   keresett;
+            int     index;
 
+            // Beolvasas
             Console.Error.Write("n = ");
             int.TryParse(Console.ReadLine(), out n);
 
-            index = 0;
             emberek = new Ember[n];
-            keresett = emberek[index];
-
             for (int i = 0; i < n; i++)
             {
                 Console.Error.Write("{0}. ember = ", i + 1);
-                string sor = Console.ReadLine();
+                string[] szuletesiDatum = Console.ReadLine().Split(" ");
 
-                string[] sorReszlet = sor.Split(" ");
-                if (sorReszlet.Length == 3)
+                int.TryParse(szuletesiDatum[0], out emberek[i].ev);
+                int.TryParse(szuletesiDatum[1], out emberek[i].ho);
+                int.TryParse(szuletesiDatum[2], out emberek[i].nap);
+            }
+
+            // Feldolgozas
+            index    = 1;
+            keresett = emberek[index - 1];
+            for (int i = 0; i < n; i++)
+            {
+                if (((emberek[i].ev * 10000) + (emberek[i].ho * 100) + (emberek[i].nap)) <
+                    ((keresett.ev * 10000) + (keresett.ho * 100) + (keresett.nap)))
                 {
-                    int.TryParse(sorReszlet[0], out emberek[i].ev);
-                    int.TryParse(sorReszlet[1], out emberek[i].ho);
-                    int.TryParse(sorReszlet[2], out emberek[i].nap);
-
-                    if (((emberek[i].ev * 10000) + (emberek[i].ho * 100) + (emberek[i].nap)) <
-                        ((keresett.ev * 10000) + (keresett.ho * 100) + (keresett.nap)) || i == 0)
-                    {
-                        keresett = emberek[i];
-                        index = i + 1;
-                    }
+                    keresett = emberek[i];
+                    index    = i + 1;
                 }
             }
 
+            // Kiiras
             Console.WriteLine(index);
         }
     }
